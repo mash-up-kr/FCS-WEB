f6e1c0d6d12b6679ab78ce06f5f097be4765e3e4
mango906 <kirby@101.inc>
2020-07-25 00:32:29 +0900

    feat: Badge 컴포넌트를 생성한다.

---
+++ src/components/common/Badge/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BadgeColors, BadgeTextColors } from './interface';
+
+interface BadgeProps {
+  children: string;
+  color: 'primary' | 'mint' | 'gray';
+}
+
+export const Badge: React.FC<BadgeProps> = (props) => {
+  const { color, children, ...restProps } = props;
+
+  return (
+    <StyledBadge color={color} {...restProps}>
+      {children}
+    </StyledBadge>
+  );
+};
+
+const StyledBadge = styled.div<BadgeProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 58px;
+  height: 24px;
+  background-color: ${(props) => BadgeColors[props.color]};
+  color: ${(props) => BadgeTextColors[props.color]};
+  font-size: 12px;
+`;
