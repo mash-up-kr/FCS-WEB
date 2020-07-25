e349e20ea1a95de88d3ca458ebb117bb8601f87b
mango906 <kirby@101.inc>
2020-07-25 17:42:37 +0900

    refactor: color.d.ts 파일 생성

---
+++ src/utils/color.d.ts
@@ -0,0 +1,18 @@
+export const white = '#FFF';
+export const black = '#000';
+
+export const gray0 = '#f8f8f9';
+export const gray1 = '#f0f0f1';
+export const gray2 = '#dfdfe0';
+export const gray3 = '#cfcfd0';
+export const gray4 = '#a8a8aa';
+export const gray5 = '#939395';
+export const gray6 = '#6b6c6e';
+export const gray7 = '#505153';
+export const gray8 = '#353537';
+export const gray9 = '#111114';
+
+export const red = '#ff6868';
+export const blue = '#3977de';
+
+export const keyColor = '#68e1de';
