3d15501008265d44046b6eda2848e2ddc56ec9b5
mango906 <kirby@101.inc>
2020-07-26 06:35:32 +0900

    refactor: Tab 컴포넌트의 불필요한 파일을 제거한다.

--- src/components/common/Tab/index.tsx
+++ src/components/common/Tab/index.tsx
@@ -1,1 +1,30 @@
-export { default } from './Tab';
+import React from 'react';
+import styled from 'styled-components';
+import tabHome from '../../../utils/assets/IC_Menu_Feed_normal.svg';
+import tabPlus from '../../../utils/assets/IC_Menu_Plus_normal.svg';
+import tabMypage from '../../../utils/assets/IC_Menu_My_normal.svg';
+// import { tabPlus, tabMypage } from '../../../views/assets';
+
+const Tab: React.FC = () => (
+  <TabContainer>
+    <Icon src={tabHome} alt="tabHome" />
+    <Icon src={tabPlus} alt="tabPlus" />
+    <Icon src={tabMypage} alt="tabMypage" />
+  </TabContainer>
+);
+
+const TabContainer = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  position: fixed;
+  bottom: 0;
+`;
+
+// TODO: Link나 A태그로 바꿔야할듯 하지만 지금은 바쁨 ㅠㅠ
+const Icon = styled.img``;
+
+export default Tab;
