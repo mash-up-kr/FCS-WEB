ae4c30e30ff2649c2797a9025253bc28e7e5b9cf
rdd9223 <rdd9223@gmail.com>
2020-08-20 15:51:33 +0900

    style: 업로드 레이아웃 설정

--- src/containers/PostContainer.tsx
+++ src/containers/PostContainer.tsx
@@ -5,8 +5,15 @@ import PostHeader from '../components/common/Header/PostHeader';
 const PostContainer: React.FC = () => (
   <Container>
     <PostHeader />
+    <UploadPhotoArea />
   </Container>
 );
+
+const UploadPhotoArea = styled.div`
+  height: 375px;
+  width: 375px;
+  background-color: gray;
+`;
 
 const Container = styled.div`
   height: 100vh;
