07d4aaa66dc1f6de77ea469e908cc8c5d8e803e4
YeongWoo <rdd9223@gmail.com>
2020-08-24 18:41:28 +0900

    feat: 업로드 헤더 구현

--- src/components/common/Header/PostHeader/index.tsx
+++ src/components/common/Header/PostHeader/index.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import { white } from '../../../../utils/color';
+import { Link } from 'react-router-dom';
+import Close from '../../../../assets/ic_navigation_close.png';
+import Back from '../../../../assets/ic_navigation_back.png';
 
 const PostHeader: React.FC = () => (
   <Wrapper>
-    <span>header</span>
+    <BackIcon to="/">
+      <Icon src={Back} />
+    </BackIcon>
+    피드 작성하기
+    <CloseIcon to="/">
+      <Icon src={Close} />
+    </CloseIcon>
   </Wrapper>
 );
-
+const Icon = styled.img``;
+const BackIcon = styled(Link)`
+  display: flex;
+  margin-left: 20px;
+`;
+const CloseIcon = styled(Link)`
+  display: flex;
+  margin-right: 20px;
+`;
 const Wrapper = styled.div`
-  width: 375px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
   height: 50px;
-  background-color: rgba(17, 17, 20, 0.05);
+  font-size: 18px;
+  font-weight: bold;
+  background-color: ${white};
 `;
 
 export default PostHeader;
