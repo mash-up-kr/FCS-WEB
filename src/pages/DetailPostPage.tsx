f1c4bf7d0eb5320ccba163a11422a1a676ab00cc
mango906 <kirby@101.inc>
2020-09-12 13:38:03 +0900

    fix: PostPage DetailPostPage로 변경

---
+++ src/pages/DetailPostPage.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import DetailPostContainer from '../containers/DetailPostContainer';
+
+class PostPage extends Component {
+  render(): JSX.Element {
+    return <DetailPostContainer />;
+  }
+}
+
+export default PostPage;
